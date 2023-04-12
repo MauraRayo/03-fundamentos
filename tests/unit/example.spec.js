98b54171cfbc29959432ddaa3310d444230a3590
Maurania Rayo <56032847+MauraRayo@users.noreply.github.com>
2023-04-12 12:56:43 +0200

    Pruebas Unitarias

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,27 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+
+
+describe( 'Example cComponent', () =>{
+test( 'Debe de ser mayor a 10', () =>{
+// arreglar
+  let value = 5;
+
+  //Estimulo
+
+  value = value + 2
+
+
+
+  //Observar el resultado
+  if(value > 10){
+    //
+  }else{
+    throw `${value} no es mayor a 10`
+  }
+
+
+
 })
+
+
+} )
